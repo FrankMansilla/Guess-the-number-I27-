ecc3fcd2a223235d925b969ffe38fd3c8ecdf5ec
frank mansilla <frankkmansilla123a@gmail.com>
2022-06-21 16:47:50 -0300

    Jinja2AndTemplateswhitStyles

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
-
 app = Flask(__name__) # create the application instance 
-
 
 
 @app.route('/') # at the end point /
 def index(): 
     return render_template('index.html') # return the index.html template
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True) # run the application
